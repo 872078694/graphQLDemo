5ffa50885d7838822d490772ca0443ff73f4291c
872078694 <34040458+872078694@users.noreply.github.com>
2021-02-04 01:03:22 -0600

    set up local connection

--- graphqldemo/graphQL/rollDiceClient.js
+++ graphqldemo/graphQL/rollDiceClient.js
@@ -1,10 +1,12 @@
+
+const fetch = require("node-fetch");
 var dice = 3;
 var sides = 6;
 var query = `query RollDice($dice: Int!, $sides: Int) {
   rollDice(numDice: $dice, numSides: $sides)
 }`;
  
-fetch('/graphql', {
+fetch('http://localhost:4000/graphql', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
